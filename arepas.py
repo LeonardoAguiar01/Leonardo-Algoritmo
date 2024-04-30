589da5bb475f2b25ecef072ca0e8e4f19e0f28a7
LeonardoAguiar01 <168542661+LeonardoAguiar01@users.noreply.github.com>
2024-04-30 07:11:45 -0400

    Add files via upload

---
+++ arepas.py
@@ -0,0 +1,7 @@
+#programa que permite hacer arepas
+h=int(input('introduzca la cantidad de tazas de harina '))
+a=int(input('introduzca la cantidad de tazas de agua '))
+s=int(input('introduzca la cantidad de cucharadas de sal '))
+m=int(input('introduzca la cantidad de cucharadas de aceite '))
+arepa=(h+a+s+m)/4
+print('la cantidad de arepas son', arepa)
